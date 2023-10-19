2f63364db1cb7af3f6226a28d8498160d3d0961e
Magical-Mist <mrudularanganatha@gmail.com>
2023-10-19 13:36:50 -0500

    ask away project commit

---
+++ README.md
@@ -0,0 +1,67 @@
+
+# Ask Away: News Research Tool 
+
+**Ask Away** is an intuitively crafted research tool, seamlessly engineered for the retrieval of information. Empowering users to input article URLs and pose inquiries, it facilitates the extraction of relevant insights from the articles with utmost ease and efficiency."
+
+
+![](ask_away.png)
+
+
+## Features:
+
+  - Enter the URLs to fetch article content.
+  - Load files from remote URLs using UnstructuredURLLoader.
+  - Construct an embedding vector using OpenAI's embeddings and leverage FAISS, a powerful similarity search library, to enable swift and effective retrieval of relevant information
+  - Interact with OpenAI's Language Model (ChatGPT) by submitting queries, and in return, obtain comprehensive answers accompanied by source URLs.
+
+
+## Installation
+
+  1. Clone this repository to your local machine using:
+
+  ```bash
+    git clone https://github.com/Magical-Mist/Article_Analyzer.git
+  ```
+  2. Navigate to the project directory:
+
+  ```bash
+    cd article_analyzer
+  ```
+  3. Install the required dependencies using pip:
+
+  ```bash
+    pip install -r requirements.txt
+  ```
+  4. Set up your OpenAI API key by creating a .env file in the project root and adding your API
+
+  ```bash
+    OPENAI_API_KEY=your_api_key_here
+  ```
+  5. Enter your user-agent in UnstructuredURLLoader's header or remove the header parameter in _main.py_ file.
+
+## How to Run?
+
+  1. Run the Streamlit app by executing:
+
+  ```bash
+    streamlit run main.py
+  ```
+  2. The web application will open in your browser.
+    - On the sidebar, you can enter the URLs directly.
+    - Initiate data loading and processing by clicking "Process URLs".
+    - The application informs each stage of processing it is undergoing, such as: text splitting, generating embedding vectors.
+    - The embeddings will be stored and indexed using FAISS, enhancing retrieval speed.
+    - The FAISS index will be saved in a local file path in pickle format for future use (faiss_index_store.pkl).
+    - Pose a question and receive an answer derived from pertinent articles.
+    - Articles used in image:
+      - https://www.livemint.com/market/stock-market-news/nifty-50-sensex-today-what-to-expect-from-stock-market-indices-in-trade-on-october-19-11697679420921.html
+      - https://www.usatoday.com/money/blueprint/investing/stock-forecast-apple/#:~:text=Is%20Apple%20a%20long%2Dterm,12%25%20upside%20from%20current%20levels.
+      - https://www.marketwatch.com/data-news/alphabet-inc-cl-a-stock-outperforms-market-despite-losses-on-the-day-36ec519a-22fefa983c23?mod=mw_quote_news
+      - https://www.fool.com/investing/2023/10/19/2-magnificent-stocks-set-to-join-alphabet-nvidia-a/
+
+## Project Structure
+
+  - main.py: The main Streamlit application script.
+  - requirements.txt: mentions the required Python packages for the project.
+  - faiss_index_store.pkl: Pickle file that stores the FAISS index.
+  - .env: Configuration file to store your OpenAI API key.
---
+++ main.py
@@ -0,0 +1,98 @@
+import os
+import streamlit as st
+import pickle
+from dotenv import load_dotenv
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.vectorstores import FAISS
+
+# Accessing all the environment variables from .env file (openai api key)
+load_dotenv()
+
+if 'urls' not in st.session_state:
+    st.session_state["urls"] = []
+    st.session_state["i"] = 1
+
+st.title("Ask Away 🔗")
+st.sidebar.title("Enter Article URLs")
+
+urls = []
+file_path = "faiss_index_store.pkl"
+
+url_container = st.sidebar.container()
+col1, col2 = st.sidebar.columns(2)
+
+if len(st.session_state['urls'])>1:
+    for url in st.session_state["urls"]:
+        url_text = url_container.text_input(f"URL {url['index']}", key=url['index'], value=url['value'])
+        st.session_state['urls'][url['index']-1]['value'] = url_text
+        urls.append(url['value'])
+elif st.session_state.i==1:
+    url = url_container.text_input(f"URL {1}", key=1)
+    st.session_state.urls = []
+    st.session_state.urls.append({'index': st.session_state.i, 'value': url})
+    urls.append(url)
+
+def add_URL(st):
+    global url_container
+    if(st.session_state.i >= 1 and st.session_state["urls"] and st.session_state["urls"][st.session_state["i"]-1]["value"]):
+        st.session_state.i += 1
+        url = url_container.text_input(f"URL {st.session_state.i}")
+        st.session_state.urls.append({'index': st.session_state.i, 'value': url})
+        urls.append(url)
+
+process_url_clicked = col1.button("Add URL", on_click=lambda: add_URL(st))
+process_url_clicked = col2.button("Process URLs")
+
+main_placeholder = st.empty()
+
+if process_url_clicked:
+    # Loading data from URLs specified
+    # browse https://httpbin.org/get to get your user-agent or remove headers
+    loader = UnstructuredURLLoader(urls=urls, headers={"User-Agent": "enter your user-agent"})
+    main_placeholder.text("Data Loading...⏳")
+    data = loader.load()
+
+    # Splitting data recursively using multiple separators
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitting in Progress...🔄")
+    docs = text_splitter.split_documents(data)
+
+    # Creating embedding using OpenAI and generating their FAISS index
+    embedding = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embedding)
+    main_placeholder.text("Building Embedding Vector...🔄")
+
+    # Saving FAISS index to a pickle file.
+    # Instead of using Vector DB, performing in-memory computation as requirement is small.
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore, f)
+main_placeholder.text("Data Processing Complete...✅")
+main_placeholder.text("")
+query = st.text_input("What are you looking for? ")
+llm = OpenAI(temperature=0.7, max_tokens=500)
+
+# Fetching related chunks of vectors and hitting OpenAi with query and individual chunks to get the result.
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result --> {"answer": "", "sources": [] }
+            st.header("Result: ")
+            st.write(result["answer"])
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")
+                for source in sources_list:
+                    st.write(source)
+
